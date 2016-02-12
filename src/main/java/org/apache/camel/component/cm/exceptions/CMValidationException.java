4c5d00f132703f96783bc55e99b5976dea92f937
oalles <oalles@neivi.es>
2016-02-12 15:28:37 +0100

    Fix. Not commited before

---
+++ src/main/java/org/apache/camel/component/cm/exceptions/CMValidationException.java
@@ -0,0 +1,30 @@
+package org.apache.camel.component.cm.exceptions;
+
+public class CMValidationException extends CMDirectException {
+
+	public CMValidationException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CMValidationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CMValidationException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CMValidationException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CMValidationException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
